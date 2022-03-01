2b01753a0c098ee10504b1b6ca83db96531f6eab
Андрей Берлин <99011598+Andrey-Berlin@users.noreply.github.com>
2022-03-01 21:55:18 +0200

    update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ section.innerHTML = `
 <br>
 <audio controls id = "audio2"></audio></li>
 <hr />
-<li> Радио Эйлат/ Музыкальное радио Израиля.
+<li> Радио Свобола/ Общественное Европейское радио.
 <br>
 <audio controls id = "audio3"></audio></li>
 <hr />
@@ -54,13 +54,18 @@ let audio1 = document.getElementById("audio1");
 audio1.innerHTML = `<source id = "source1" type="audio/mpeg">`;
 let audio2 = document.getElementById('audio2');
 audio2.innerHTML = `<source id = "source2" type="audio/mpeg">`;
+let audio3 = document.getElementById('audio3');
+audio2.innerHTML = `<source id = "source3" type="audio/mpeg">`;
 /*audio.style.fontSize = '50px';*/
 
-let links = ['https://kanliveicy.media.kan.org.il/icy/kanreka_mp3','https://emgregion.hostingradio.ru:8064/moscow.europaplus.mp3'];
+let links = ['https://kanliveicy.media.kan.org.il/icy/kanreka_mp3','https://emgregion.hostingradio.ru:8064/moscow.europaplus.mp3','https://n0d.radiojar.com/hcrb063nn3quv?rj-ttl=5&rj-tok=AAABf0cR0TcAuolkyeDIQy7HGg'];
 
 let source1 = document.getElementById("source1");
 source1.src = links[0];
 let source2 = document.getElementById('source2');
 source2.src = links[1];
+let source3 = document.getElementById('source3');
+source2.src = links[2];
 }
+
 audioPlayer();
